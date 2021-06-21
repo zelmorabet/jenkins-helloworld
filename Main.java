49df9aee8de01075a4cc0702beb20bb9f2b63697
zelmorabet <61009201+zelmorabet@users.noreply.github.com>
2021-06-21 09:11:44 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Zakaria1234!");
+        System.out.println("Hello, ZakariaELMORABET!");
     }
 }
 
